96ee3546970f9db1a34153e82cd39d8b48913931
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 20:02:23 -0500

    Create Loop Control.py

---
+++ Day 13/Loop Control.py
@@ -0,0 +1,11 @@
+# Loop Control Statements = change a loop execution from its normal sequence
+
+# break =      used to terminate the loop entirely
+# continue = skips to the next iteration of the loop
+# pass =      doead nothing, acts as a placeholder
+
+# BREAK
+while True:
+    name = input("Enter your name: ")
+    if name != "":
+        break 
