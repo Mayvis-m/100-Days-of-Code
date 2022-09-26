49c27143c5e2fe59d1d7b19abc1a0be420e5bc9b
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 20:05:58 -0500

    Add files via upload

---
+++ Day 14/sets.py
@@ -0,0 +1,16 @@
+# sets = collection which is unordered, undindexed. No duplicate values
+
+utensils = {"fork", "spoon", "knife"}
+dishes = {"bowel", "plate", "cup", "knife"}
+
+# utensils.add("napkin")
+# utensils.remove("fork")
+# utensils.clear()
+# dishes.update(utensils)
+# dinner_table = utensils.union(dishes)
+
+# print(utensils.difference(dishes))  # see what utensils has that dishes doesn't  
+print(utensils.intersection(dishes))  # see what utensils has that dishes has 
+
+# for x in dishes:
+#     print(x)
