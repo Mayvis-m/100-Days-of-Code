e5ae7910820a821b16a4202d4651355a248a14e2
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 19:18:37 -0500

    Add files via upload

---
+++ Day1-3.py
@@ -0,0 +1,24 @@
+# STRINGS (str)
+first_name = "Mayvis"
+last_name = "Underscore"
+full_name = first_name +" "+ last_name
+
+print("Hello "+ full_name)
+
+#print(type(name))
+
+# INTEGERS (int)
+age = 100
+age += 1
+print("Your age is: "+str(age)) 
+#print(type(age))
+
+# FLOAT
+height = 250.5
+print("Your height is: "+str(height)+"cm")
+#print(type(height))
+
+# BOOLEAN
+human = True
+print("Are you a human: "+str(human))
+# print(type(human))
