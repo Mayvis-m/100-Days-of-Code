1710291ced571cbea3fda79e6956ae7ccd7665b2
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 19:54:42 -0500

    Create For Loop.py

---
+++ Day 12/For Loop.py
@@ -0,0 +1,14 @@
+# for loop = a statment that will execute it's block of code
+#            a limited amount of times
+
+#            while loop = unlimited
+#            for loop = limited
+
+# for index  in range (10):
+#     print(index+1)
+
+# for index in range(50,100):  # first number (50) is starting point, second number (100) last number
+#     print(index)             # add (100 +1) to include 100
+
+for index in range(50,100+1,2):
+    print(index)
