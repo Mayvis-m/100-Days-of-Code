b018a765bd6eca12e3263103a2b504c12f51b30a
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 20:03:49 -0500

    Add files via upload

---
+++ Day 13/Lists.py
@@ -0,0 +1,10 @@
+# list = used to store multiple items in a single variable
+
+food = ["pizza", "hamburger", "hot dog", "ramen"]
+
+food[0] = "sushi"
+
+# print(food[0])
+
+for x in food:
+    print(x)
