e09822aba243a6ed0734146e49d9a2194216a20a
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 19:55:07 -0500

    Add files via upload

---
+++ Day 12/For Loop1.py
@@ -0,0 +1,8 @@
+# for loop = a statment that will execute it's block of code
+#            a limited amount of times
+
+#            while loop = unlimited
+#            for loop = limited
+
+for index in "Mayvis Underscore":
+    print(index)
