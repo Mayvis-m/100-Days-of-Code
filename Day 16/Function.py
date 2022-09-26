cae6e4a2acf5078b4952e3428d39f7c9e7be4200
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 20:08:25 -0500

    Create Function.py

---
+++ Day 16/Function.py
@@ -0,0 +1,14 @@
+# function = a block of code which is execited only when it is called.
+
+# def hello(name):
+#     print("Hello! " +name)
+#     print("Have a nice day!")
+
+# hello("Mayvis")
+
+def hello(first_name, last_name,age):
+    print("Hello " +first_name+ " " +last_name)
+    print("You are " +str(age)+ " years old")
+    print("Have a nice day!")
+
+hello("Mayvis","Underscore",101)
