e8dfeff42b166a9a55c9e68619e50fe3aff64c53
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 19:46:44 -0500

    Create Math Module.py

---
+++ Day 8/Math Module.py
@@ -0,0 +1,15 @@
+#MATH MODULE
+import math 
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+# print(round(pi))
+# print(math.ceil(pi))
+# print(math.floor(pi))
+# print(abs(pi))
+# print(math.sqrt(pi))
+# print(max(x,y,z))
+# print(min(x,y,z))
