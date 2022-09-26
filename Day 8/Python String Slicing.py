06c7860c2a5a151bad3c14be9efdfe08f2700339
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 19:47:15 -0500

    Add files via upload

---
+++ Day 8/Python String Slicing.py
@@ -0,0 +1,21 @@
+# PYTHON STRING SLICING
+# slicing = create a substring by extracting elements from another string
+#           indexing[] or slice()
+#           [start:stop:step]
+
+# Reminder: computers always start at 0 !!!
+
+#INDEXING[]
+name = "Mayvis Underscore"
+
+# first_name = name[3]
+
+first_name = name[0:3]
+last_name = name [7:17]
+funky_name = name[0:17:2]
+reversed_name = name[::-1]
+#                [start:stop:step]
+
+# print(first_name)
+print(reversed_name )
+
