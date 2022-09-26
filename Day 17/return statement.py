f8506d654e2fca7e2666a5d1a873f0a50f49ff92
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-09-25 20:10:14 -0500

    Add files via upload

---
+++ Day 17/return statement.py
@@ -0,0 +1,10 @@
+# return statement = Functions send Pythong values/objects back to the caller.
+#                    Theses values/objects are known as the function's return value
+
+def multiply(number1, number2):
+    result = number1 * number2
+    return result
+
+x = multiply(6,8)
+
+print(x)
