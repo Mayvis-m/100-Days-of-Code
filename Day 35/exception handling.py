72430a734fe9caf37707f8fac500732cdcf178c0
Margaret <88308906+Mayvis-m@users.noreply.github.com>
2022-10-26 02:10:20 -0500

    Create exception handling.py

---
+++ Day 35/exception handling.py
@@ -0,0 +1,15 @@
+# exception = events detected during execution that interrupt the flow of a program
+
+try: 
+    numerator = int(input("Enter a number to divide: "))
+    denominator = int(input("Enter a number to divide by: "))
+    result = numerator / denominator
+    print(result)
+except ZeroDivisionError:
+    print("You can't divide by zero!")
+except ValueError:
+    print("Enter only numbers please.")
+except Exception:
+    print("something went worng :'(((( ")
+finally:
+    print("This will always execute.")
